aeafed9a83cf5314ccea8f7ed1e7b36e0eb9f9b7
StormQuant User <alex@stormquant.com>
2026-06-22 19:48:43 -0700

    used some duplicate code to save pngs of the output

--- visualization.py
+++ visualization.py
@@ -47,8 +47,18 @@ class VisualizeFrame(tk.Frame):
         self.grid_propagate(False)
         # self.image_label = tk.Label(self, text = "Image")
         # self.image_label.grid(column=0, row=0, sticky="nsew")
-        self.create_plot_button = tk.Button(self, text="Plot", command= lambda: self.create_plot())
+
+
+        self.button_frame = tk.Frame(self)
+        self.button_frame.grid(column=2, row=0, sticky="nsew")
+        self.button_frame.configure(bg="white")
+        # self.button_frame.rowconfigure(0, weight=1)
+        # self.button_frame.rowconfigure(1, weight=1)
+
+        self.create_plot_button = tk.Button(self.button_frame, text="Plot", command= lambda: self.create_plot())
         self.create_plot_button.grid(column=2, sticky="new")
+        self.create_images_button = tk.Button(self.button_frame, text="Create Images", command= lambda: self.create_images())
+        self.create_images_button.grid(column=2, sticky="new")
     #
     # def open_image(self):
     #     file_path = filedialog.askopenfilename(title="Open Image files", filetypes = (("PNG File","*.png"),))
@@ -61,6 +71,81 @@ class VisualizeFrame(tk.Frame):
     #     photo = ImageTk.PhotoImage(image_resized)
     #     self.image_label.config(image = photo)
     #     self.image_label.photo = photo
+
+    def create_images(self):
+        def convertRawFile(folder, filename):
+            # bashCommand = "/home/jake/Documents/stormquant-beta/cmake-build-debug/common/raytestingutils/convertRawToCleartext "
+            bashCommand = "/home/sq/sq/stormquant-beta/build_old/common/raytestingutils/convertRawToCleartext "
+            # bashCommand = "/home/sq/Desktop/pythonAppImprovements/testingFiles/binFiles "
+            bashCommand = bashCommand + "-d " + folder + " -f " + filename
+            process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+            output, error = process.communicate()
+
+        folders = ["./scripts"]
+
+        for mainfolder in folders:
+            output = []
+            for folder in np.sort(glob.glob(mainfolder)):
+                for index, file in enumerate(np.sort(glob.glob(folder + "/*.bin"))[:]):
+                    print(folder, '/' + file.split('/')[-1])
+                    convertRawFile(folder, '/' + file.split('/')[-1])
+
+            legenedArr = []
+            freq = np.fft.fftfreq(6000, d=1.0 / 25e6)
+            for folder in np.sort(glob.glob(mainfolder)):
+                for index, file in enumerate(np.sort(glob.glob(folder + "/*_x.csv"))):
+                    metaFile = file.split('_x')[0] + "_meta.txt"
+                    xfile = file
+                    yfile = file.split('_x')[0] + "_y.csv"
+                    with open(metaFile, 'r') as fid:
+                        lines = fid.readlines()
+                    data = lines[0].split(',')
+                    el = float(data[0])
+                    az = float(data[1])
+                    az = 0.0
+                    el = 0.0
+                    folderName = os.getcwd().split('/')[-1]
+                    for csvI, csvFile in enumerate([xfile, yfile]):
+                        fig, ax = plt.subplots(3, 1)
+                        df = pd.read_csv(csvFile, header=None).transpose()
+                        ax[0].plot(df[0])
+                        for i in range(50):
+                            ax[0].plot([i * 50, i * 50], [-20000, 20000], 'k--')
+                        ax[0].set_xlim([100, 800])
+                        ax[0].set_ylim([-5000, 5000])
+                        ax[1].plot(df[0])
+                        ax[1].set_ylim(-2500, 2500)
+                        # cutOutIndex = [1790, 1830]
+                        # cutOutDifference = cutOutIndex[1] - cutOutIndex[0]
+                        # freq2 = np.fft.fftfreq(cutOutDifference, d=1.0 / 25e6)
+                        # ax3 = ax[2].twinx()
+                        ax[2].plot(freq, np.fft.fft(df[0]))
+                        # ax3.plot(freq2, np.fft.fft(df[0][cutOutIndex[0]:cutOutIndex[1]]), 'r--')
+                        # legenedArr.append(file)
+                        # ax[0].legend(legenedArr)
+
+                        # HIS NAME
+                        # fig.suptitle(folderName + csvFile + " az: " + str(az) + " el: " + str(el))
+                        # MY NAME FOR FILE?
+                        now = datetime.now()
+                        formatted_time = now.strftime("%m-%d %H:%M:%S")
+                        fig.suptitle(formatted_time + " az: " + str(az) + " el: " + str(el))
+                        fig.set_size_inches([15, 15])
+                        if csvI == 0:
+                            extension = 'x'
+                        else:
+                            extension = 'y'
+
+                        ## CUT FOR SAVING FILE
+                        outputFilename = formatted_time + f'%03d' % index + extension + '.png'
+                        # print(outputFilename)
+                        fig.savefig(outputFilename)
+
+
+                        plt.close(fig)
+                        os.system('rm ' + csvFile)
+        print("HELLO WORLD")
+        os.system('rm *.csv')
 
     def create_plot(self):
        # script_path = Path("./scripts/testingviz.py")
@@ -175,16 +260,16 @@ class VisualizeFrame(tk.Frame):
 
 
 
-
-class ControlFrame(tk.Frame):
-    def __init__(self, parent):
-        super().__init__(parent, background = 'blue')
-        self.grid_propagate(False)
-        self.create_plot_button = tk.Button(self, text="Plot", command= lambda: self.create_plot())
-        self.create_plot_button.grid(column=0, row=0, sticky="nsew")
-
-        self.images_button = tk.Button(self, text="IMAGES", command=lambda: parent.visualization_frame.open_image())
-        self.images_button.grid(column=0, row=1, sticky="nsew")
+#
+# class ControlFrame(tk.Frame):
+#     def __init__(self, parent):
+#         super().__init__(parent, background = 'blue')
+#         self.grid_propagate(False)
+#         self.create_plot_button = tk.Button(self, text="Plot", command= lambda: self.create_plot())
+#         self.create_plot_button.grid(column=0, row=0, sticky="nsew")
+#
+#         self.images_button = tk.Button(self, text="IMAGES", command=lambda: parent.visualization_frame.open_image())
+#         self.images_button.grid(column=0, row=1, sticky="nsew")
 
     # def create_plot(self):
     #    # script_path = Path("./scripts/testingviz.py")
