6a669465a9bb043c2f61f64f079fb8e74e7046c6
StormQuant User <alex@stormquant.com>
2026-07-28 18:40:49 -0700

    add slash to end of scp.get local path

--- fpga_layout.py
+++ fpga_layout.py
@@ -454,7 +454,7 @@ class ButtonFrame(tk.Frame):
 
 
             with SCPClient(client.get_transport()) as scp:
-                scp.get(f"raw-bin-{formatted_time}.tgz", "./generatedBinFiles")
+                scp.get(f"raw-bin-{formatted_time}.tgz", "./generatedBinFiles/")
         except Exception as e:
             print(f"An error has occurred: {e}")
 
