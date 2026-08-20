8b314b3c19cb5d43d5752ffb467ae0a887e940dc
StormQuant User <alex@stormquant.com>
2026-08-20 13:50:16 -0700

    path for cleartext

--- visualization.py
+++ visualization.py
@@ -122,7 +122,7 @@ class VisualizeFrame(tk.Frame):
             # todo find correct location on target computer
             #bashCommand = "/home/sq/sq/stormquant-beta/build_old/common/raytestingutils/convertRawToCleartext "
             bashCommand = f"{os.environ.get('CLEAR_TEXT_PATH')} "
-            bashCommand = "./ClearText/convertRawToCleartext "
+            bashCommand = "../ClearText/convertRawToCleartext "
             #bashCommand = f"{cpp_module} "
             bashCommand = bashCommand + " -d " + folder + " -f " + filename
             process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
@@ -232,7 +232,7 @@ class VisualizeFrame(tk.Frame):
            #bashCommand = "/home/sq/sq/stormquant-beta/build_old/common/raytestingutils/convertRawToCleartext "
            #bashCommand = f"{os.environ.get('CLEAR_TEXT_PATH')} "
            #bashCommand = f"{cpp_module} "
-           bashCommand = "./ClearText/convertRawToCleartext "
+           bashCommand = "../ClearText/convertRawToCleartext "
            bashCommand = bashCommand + "-d " + folder + " -f " + filename
            process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
            output, error = process.communicate()
