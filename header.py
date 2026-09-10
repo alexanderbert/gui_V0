ff6e3cafe505e1005f99c776e91c1a7efd811d26
StormQuant User <alex@stormquant.com>
2026-09-10 14:52:06 -0700

    add connection toggle to header for ease of use

--- header.py
+++ header.py
@@ -4,6 +4,8 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import PhotoImage
 import os
+import state
+from network_toggle import NetworkSwitch
 
 def resource_path(relative_path):
     try:
@@ -19,6 +21,24 @@ class HeaderFrame(tk.Frame):
         self.header_image = None
         self.grid_propagate(False)
 
+        self.network_switch = NetworkSwitch(self, label_a="Wifi", label_b="Ethernet", command=self.on_switch_toggle)
+        self.network_switch.pack(side="right", padx=50, anchor="s")
+
+    def on_switch_toggle(self, is_on):
+        if is_on:
+            # print("Switch is at Position B")
+            # print(f"on: {self.network_state.get()}")
+            # state.network_state = "Ethernet"
+            print(state.network_state)
+            # return state.network_state
+        else:
+            # print("Switch is at Position A")
+            # print(f"off: {self.network_state.get()}")
+            # state.network_state = "Wifi"
+            print(state.network_state)
+            # return state.network_state
+
+
     def add_header_image(self):
         self.header_image = PhotoImage(file=resource_path("images/logoresize3.png"))
         image_label = tk.Label(self, image=self.header_image, borderwidth=0)
@@ -26,3 +46,4 @@ class HeaderFrame(tk.Frame):
         image_label.image = self.header_image
         title_text = tk.Label(self, text = "Calibration Tool", font=("Arial", 20, 'bold'))
         title_text.pack(side="right", padx=50)
+
