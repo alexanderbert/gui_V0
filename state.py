fed7b7d00c15ba330164d5b80594b600b739da09
StormQuant User <alex@stormquant.com>
2026-09-11 14:44:04 -0700

    add field values to state

--- state.py
+++ state.py
@@ -7,3 +7,11 @@ load_dotenv()
 print(f"Host IP: {os.environ.get('HOST_IP')}")
 network_state = f"{os.environ.get('HOST_IP')}"
 
+starting_azimuth_value = 0
+ending_azimuth_value = 0
+starting_el_value = 0
+ending_el_value = 0
+speed_value = 20
+increment_value = 1.5
+
+
