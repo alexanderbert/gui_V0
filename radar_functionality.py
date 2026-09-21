3ccf74c22c00dc5b25b6a3c2886c7eed1d32cc03
StormQuant User <alex@stormquant.com>
2026-09-21 13:23:20 -0700

    comment out if capture check for now

--- radar_functionality.py
+++ radar_functionality.py
@@ -289,11 +289,12 @@ class RadarFunctionality(tk.Frame):
                         data = channel.recv(1024).decode("iso-8859-1")
                         #TODO EDIT this for a visual cue that the fpga is finished the_jake_equation
                         # if f"Captured {expected_Q_Value} packets." in data:
-                        if f"Captured" in data or "packets" in data:
-                            self.az_entry.delete(0, tk.END)
-                            self.az_entry.insert(tk.END, "RUN FINISHED")
-                            is_fpga_running = False
-                            break
+                        # if f"Captured" in data or "packets" in data:
+                        #     self.az_entry.delete(0, tk.END)
+                        #     self.az_entry.insert(tk.END, "RUN FINISHED")
+                        #     is_fpga_running = False
+                        #     break
+                        print(buffer)
                         buffer += data
                         while True:
                             position_match = position_pattern.search(buffer)
