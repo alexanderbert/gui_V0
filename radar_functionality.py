138258520dc9f6330d9963da3342d33eb36072cd
StormQuant User <alex@stormquant.com>
2026-09-21 17:03:12 -0700

    remove print

--- radar_functionality.py
+++ radar_functionality.py
@@ -314,7 +314,6 @@ class RadarFunctionality(tk.Frame):
 
                     if channel.recv_ready():
                         data = channel.recv(1024).decode("iso-8859-1")
-                        print(repr(data))
                         buffer += data
                         captured_match = re.search(r"Captured\s+(\d+)\s+packets\.", buffer)
                         if captured_match:
