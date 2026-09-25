fe51ff9fee81c55e7b86f35e59d0d8c3baf027cf
alexanderbert <alex@stormquant.com>
2026-09-25 14:42:42 -0700

    remove print function

--- radar_functionality.py
+++ radar_functionality.py
@@ -361,7 +361,7 @@ class RadarFunctionality(tk.Frame):
                         #PUTTING RAW DATA INTO RAW CSV
                         self.raw_csv_queue.put(data)
                         buffer += data
-                        print(repr(buffer))
+                        #print(repr(buffer))
                         captured_match = re.search(r"Captured\s+(\d+)\s+packets\.", buffer)
                         if captured_match:
                             captured_count = int(captured_match.group(1))
